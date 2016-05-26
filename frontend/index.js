e1d86c14f6b94e34a60b44ae3ea5608b9f2d4827
namlook <nicolas.clairon@elkorado.com>
2016-05-26 13:21:47 +0200

    move from hashHistory to browserHistory

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, hashHistory } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import configureStore from './store';
 
@@ -14,6 +14,6 @@ const store = configureStore();
 
 render(
   <Provider store={store}>
-    <Router history={hashHistory} routes={routes} />
+    <Router history={browserHistory} routes={routes} />
   </Provider>, document.getElementById('app')
 );
